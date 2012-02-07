09045fd477cd608c011f4d9dfc64e6b2171c2577
root <root@MAURODELLLINUX.(none)>
2012-02-07 10:24:27 -0200

    criação do projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AnimalHouse::Application.config.secret_token = 'a1ffc73b1e1e096ccd73c36922f7a911e70fab04cbbb939ed089a96a093460512623caa83ad6b4da6fde075f3d087dd5cfdfc97b045a418e052cd5c7941ab22d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+AnimalHouse::Application.config.session_store :cookie_store, key: '_AnimalHouse_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# AnimalHouse::Application.config.session_store :active_record_store
